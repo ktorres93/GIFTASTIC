f3506b605001c02734d18d56d825694fdd02f590
Kevin <ktorres93@knights.ucf.edu>
2017-06-05 19:26:51 -0400

    restarting javascript with only the array and doc ready

---
+++ assets/javascript/logic.js
@@ -0,0 +1,8 @@
+// array of original buttons
+var natureArr = ["beach", "jungle", "mountain", "ocean", "volcano",
+    "waves", "desert", "river", "woodlands"
+];
+var animate = 0;
+// ready the doc
+$(document).ready(function() {
+         
