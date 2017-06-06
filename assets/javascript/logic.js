bed26411b1d3cbd76d6c6804b2cc434cf41d0ae8
Kevin <ktorres93@knights.ucf.edu>
2017-06-05 21:25:46 -0400

    javascript linked

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,8 +1,13 @@
-// array of original buttons
-var natureArr = ["beach", "jungle", "mountain", "ocean", "volcano",
-    "waves", "desert", "river", "woodlands"
-];
-var animate = 0;
+
 // ready the doc
 $(document).ready(function() {
+//console.log("CHECK");
+
+// array of original buttons
+var natureArr = ["beach", "jungle", "mountain", "ocean", "volcano","waves", "desert", "river", "woodlands"];
+for (var i = 0; i < natureArr.length; i++) {
+    //console.log(natureArr[i]);
+}
+});
+
          
